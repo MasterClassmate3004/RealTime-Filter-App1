fa334a4886dcc1a213eba3a65b92cb012e0bf923
MasterClassmate3004 <77794181+MasterClassmate3004@users.noreply.github.com>
2021-07-07 20:23:47 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,20 @@
+function preload()
+{
+
+}
+
+function setup()
+{
+    canvas = createCanvas(300 , 300);
+}
+
+
+function draw()
+{
+
+}
+
+function takeSnapshot()
+{
+    save('RealTime_Filter.png');
+}
